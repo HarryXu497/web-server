dd0b3937fb4e76721e2251b8ed9f17bf14c6d07b
HarryXu497 <harryxu497@gmail.com>
2023-06-11 10:36:40 -0400

    added demos

--- problems/2/Main.java
+++ problems/2/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println(s.nextLine());
+        
+        s.close();
     }
 }
