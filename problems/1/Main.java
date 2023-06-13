3a7b954adde83f5a6893af9e4d608f8160979aa9
HarryXu497 <harryxu497@gmail.com>
2023-06-13 10:23:07 -0400

    updated styles

--- problems/1/Main.java
+++ problems/1/Main.java
@@ -1,14 +1,5 @@
-import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        
-        long A = input.nextLong();
-        long B = input.nextLong();
-        
-        System.out.println(A + B);
-        
-        input.close();
+        System.out.println("Hello World");
     }
 }
