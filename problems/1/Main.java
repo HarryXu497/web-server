6d4d6beed5f49dd5242f3a503055a340c5f1081b
HarryXu497 <harryxu497@gmail.com>
2023-06-13 15:16:30 -0400

    fixed type

--- problems/1/Main.java
+++ problems/1/Main.java
@@ -1,5 +1,1 @@
-class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
+What are you doing?
--- problems/2/Main.java
+++ problems/2/Main.java
@@ -1,18 +1,5 @@
-import java.util.*;
-public class Main {
-    static final int mod = 1000000007, MN = 1004;
-    static long N, M, f = 1;
-    static long[][] dp = new long[MN][MN];
+class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextLong();
-        M = sc.nextLong();
-        if(M == 0 || N > M) { System.out.print(0); return; }
-        for(int i=1; i<=N; i++) f = (f * i) % mod;
-        dp[0][0] = 1; dp[1][1] = 1;
-        for(int i=1; i<=M; i++)
-            for(int j=1; j<=N; j++)
-                dp[i][j] = (dp[i-1][j-1] + (dp[i-1][j] * j) % mod) % mod;
-        System.out.print((dp[(int)M][(int)N] * f) % mod);
+        System.out.println(14);
     }
 }
